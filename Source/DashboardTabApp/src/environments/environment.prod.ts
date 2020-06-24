c028f48fd1d560ba69ecdf16ab2dbecafbf1e0be
mwillswork <62391973+mwillswork@users.noreply.github.com>
2020-06-24 09:47:11 +0100

    Update environment.prod.ts

--- Source/DashboardTabApp/src/environments/environment.prod.ts
+++ Source/DashboardTabApp/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
 
     authConfig: {
         instance: "https://login.microsoftonline.com/",
-        tenantId: "7520da05-6887-4d9d-9876-45bbf886ac78",
+        tenantId: "change",
         clientId: "62a0eea7-3c79-450e-8083-ab31364d45a3",
         redirectUri: "/app-silent-end",
         cacheLocation: "localStorage",
