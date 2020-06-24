8b742b9b273fb56d55fb4d3b4060b82a8c2090af
mwillswork <62391973+mwillswork@users.noreply.github.com>
2020-06-24 10:34:59 +0100

    Update environment.prod.ts

--- Source/QuestionsTabApp/src/environments/environment.prod.ts
+++ Source/QuestionsTabApp/src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://qbotdemotesting.azurewebsites.net/api/Request/",
-    selfUrl: "https://qbotdemotesting-questions.azurewebsites.net",
+    apiBaseUrl: "https://nclqbot.azurewebsites.net/api/Request/",
+    selfUrl: "https://nclqbot-questions.azurewebsites.net",
 
     authConfig: {
         instance: "https://login.microsoftonline.com/",
         tenantId: "7520da05-6887-4d9d-9876-45bbf886ac78",
-        clientId: "62a0eea7-3c79-450e-8083-ab31364d45a3",
+        clientId: "e976602e-4e43-4d96-ab82-fc98742bc4d0",
         redirectUri: "/app-silent-end",
         cacheLocation: "localStorage",
         navigateToLoginRequestUrl: false,
