05688699971bc3a8b34c6ebe5351c3c0267962cb
mwillswork <62391973+mwillswork@users.noreply.github.com>
2020-06-24 10:32:55 +0100

    Update environment.prod.ts

--- Source/DashboardTabApp/src/environments/environment.prod.ts
+++ Source/DashboardTabApp/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://qbotdemotesting.azurewebsites.net/api/Request/",
+    apiBaseUrl: "https://nclqbot.azurewebsites.net/api/Request/",
 
     authConfig: {
         instance: "https://login.microsoftonline.com/",
-        tenantId: "7520da05-6887-4d9d-9876-45bbf886ac78",
-        clientId: "62a0eea7-3c79-450e-8083-ab31364d45a3",
+        tenantId: "9c5012c9-b616-44c2-a917-66814fbe3e87",
+        clientId: "e976602e-4e43-4d96-ab82-fc98742bc4d0",
         redirectUri: "/app-silent-end",
         cacheLocation: "localStorage",
         navigateToLoginRequestUrl: false,
